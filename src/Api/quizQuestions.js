54dc94624afe9a34d338bd0c4d69dc842cb091ad
b00tk1ll <guilherme.m.oto@hotmail.com>
2020-03-24 19:17:08 -0300

    Add perguntas e repostas

--- src/Api/quizQuestions.js
+++ src/Api/quizQuestions.js
@@ -1,52 +1,486 @@
 var quizQuestions = [
     {
-        question: "Pergunta 1",
+        question: "Qual sua idade?",
         answers: [
             {
                 type: "Final-1",
-                content: "Resposta 1"
+                content: "0-12"
             },
             {
+                type: "Final-0",
+                content: "12-60"
+            },
+           {
                 type: "Final-2",
-                content: "Resposta 2"
-            },
-            {
-                type: "Final-3",
-                content: "Resposta 3"
+                content: "60+"
             }
         ]
     },
     {
-        question: "Pergunta 2",
+        question: "Gênero?",
         answers: [
             {
-                type: "Final-1",
-                content: "Resposta 1"
+                type: "Final-0",
+                content: "M"
             },
             {
-                type: "Final-2",
-                content: "Resposta 2"
-            },
-            {
-                type: "Final-3",
-                content: "Resposta 3"
+                type: "Final-0",
+                content: "F"
             }
         ]
     },
     {
-        question: "Pergunta 2",
+        question: "É gestante?",
         answers: [
             {
                 type: "Final-1",
-                content: "Resposta 1"
+                content: "Sim"
             },
             {
-                type: "Final-2",
-                content: "Resposta 2"
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Possui doençãs renais?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
             },
             {
-                type: "Final-3",
-                content: "Resposta 3"
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Possui doenças cardiovasculares?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Possui diabetes?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Já fez quimioterapia?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Possui Hipertensão?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Possui alguma doença respiratória?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "É fumante?(qualquer tipo, cigarro comum, cigarro de palha, maconha, vape, narguile)",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Possui alguma doença autoimune?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Tem a imunidade comprometida/suprimida?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Fez cirurgia nos últimos 6 meses?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Está com febre?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Está com coriza?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Está com o nariz entupido?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Possui cansaço respiratório?(leve dificuldade para respirar)",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Tem tosse seca?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Tem dor de cabeça?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Tem dores no corpo?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Tem mal estar em geral?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Está com dor de garganta?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Está com diarréia?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Tem incapacidade se sentir cheiros e/ou sabores?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Está vomitando?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Está tendo convulsões?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },    
+    {
+        question: "Está tendo dificuldade para respirar com dor ou pressão no peito?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },    
+    {
+        question: "Está notando as suas extremidades azuladas?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },    
+    {
+        question: "Está notando o rosto ou os lábios azulados?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },    
+    {
+        question: "Tomou algum medicamento para febre?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },    
+    {
+        question: "Mesmo com uso de medicamentos os sintomas persistem após 2h?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },    
+    {
+        question: "Tomou algum medicamento para Vômito?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },    
+    {
+        question: "Mesmo com uso de medicamentos os sintomas persistem?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Tomou algum medicamento para o desconforto respiratório?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },
+    {
+        question: "Mesmo com uso de medicamentos os sintomas persistem?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },    
+    {
+        question: "Teve contato com algum caso suspeito de ter a COVID-19?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
+            }
+        ]
+    },    
+    {
+        question: "Viajou para o exterior nos últimos 2 meses?",
+        answers: [
+            {
+                type: "Final-1",
+                content: "Sim"
+            },
+            {
+                type: "Final-0",
+                content: "Não"
             }
         ]
     }
